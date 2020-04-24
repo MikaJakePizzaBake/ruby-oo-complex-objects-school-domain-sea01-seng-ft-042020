b05a3231f07469aea49d0362697089747410512f
Trevor Low <tlow94@uw.edu>
2020-04-24 03:02:10 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,10 +1,11 @@
 require 'pry'
 class School
-  attr_accessor :roster, :grade
+  attr_accessor :roster, :grade, :student
   def initialize (roster)
     @roster = {}
   end
   def add_student (student, grade)
+    roster[:grade] = student
 
   end
 
